eb7e159b136e9429be8fde692b549fbbed8b3ba1
Attila Bagossy <bagossyattila@outlook.com>
2025-05-21 15:26:37 +0200

    feat: started the clickity app

---
+++ javafx-clickity/src/main/java/clickity/ClickityApplication.java
@@ -0,0 +1,11 @@
+package clickity;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClickityApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
---
+++ javafx-clickity/src/main/java/clickity/Main.java
@@ -0,0 +1,12 @@
+package clickity;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(
+                ClickityApplication.class,
+                args
+        );
+    }
+}
